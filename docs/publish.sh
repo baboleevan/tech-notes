f23956c872c8bd88427e91de652f33b5c168f016
이헌기 <lee.hk@hwikgo.com>
2022-11-30 22:41:57 +0900

    update docs

---
+++ docs/publish.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# remove gitbook old things
+rm -rf _book
+rm -rf docs
+
+# gitbook init
+gitbook install && gitbook build
+
+# build pages
+mkdir docs
+cp -R _book/* docs/
+
+cp SUMMARY.md README.md
+
+# delete things
+git clean -fx _book
+
+# upload
+git add .
+git commit -a -m "update docs"
+git push -u origin main
